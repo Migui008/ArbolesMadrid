e3b3df5c80beafea6b7d7a39c4a808aaffcc68b3
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-11-11 06:45:01 +0100

    Create common_data.php

---
+++ common_data.php
@@ -0,0 +1,16 @@
+<?php
+$arboles_filter=[
+  "clase",
+  "familia",
+  "altura",
+  "hoja",
+  "visitas"
+];
+
+$parques_filter=[
+  "zona o distrito",
+  "tipo", //histórico, urbano, jardín botánico, parque forestal
+  "accesibilidad", //bus, metro, renfe
+  "visitas"
+]
+?>
