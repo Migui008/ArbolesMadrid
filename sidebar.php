c050c46006e49dab42827f87fec7779c842ae757
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-11-11 06:24:04 +0100

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,12 @@
+<div id="sidebar">
+  <div id="sidebar_mostVisited">
+    <?php
+    //mostVisitedArray = getMostVisited();
+    foreach(/*mostVisitedArray as id*/){
+      ?>
+      <div class=""></div>
+      <?php
+    }
+    ?>
+  </div>
+</div>
