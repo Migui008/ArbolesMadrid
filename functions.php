9fdc4a0588d15893c2a175835d989be66f12eaa0
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-11-11 05:19:32 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,63 @@
+function incrementarVisitasArbol($arbol_id) {
+    require_once("bbddconnect.php");
+    global $conn;
 
+    try {
+        $conn->beginTransaction();
+
+        $stmt = $conn->prepare("SELECT visitas FROM arboles WHERE id_arbol = :arbol_id");
+        $stmt->bindParam(':arbol_id', $arbol_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $visitas = $stmt->fetchColumn();
+
+        $visitas++;
+
+        $stmt = $conn->prepare("UPDATE arboles SET visitas = :visitas WHERE id_arbol = :arbol_id");
+        $stmt->bindParam(':visitas', $visitas, PDO::PARAM_INT);
+        $stmt->bindParam(':arbol_id', $arbol_id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        $conn->commit();
+        return $visitas;
+    } catch (Exception $e) {
+        $conn->rollBack();
+        echo "Failed: " . $e->getMessage();
+    }
+    $conn = null;
+}
+
+function incrementarVisitasParque($parque_id) {
+    require_once("bbddconnect.php"); 
+  	global $conn;
+
+    try {
+        // Iniciar una transacción
+        $conn->beginTransaction();
+        
+        // Obtener las visitas actuales
+        $stmt = $conn->prepare("SELECT visitas FROM parques WHERE id_parque = :parque_id");
+        $stmt->bindParam(':parque_id', $parque_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $visitas = $stmt->fetchColumn();
+
+        // Incrementar el contador de visitas
+        $visitas++;
+
+        // Actualizar el campo visitas en la tabla parques
+        $stmt = $conn->prepare("UPDATE parques SET visitas = :visitas WHERE id_parque = :parque_id");
+        $stmt->bindParam(':visitas', $visitas, PDO::PARAM_INT);
+        $stmt->bindParam(':parque_id', $parque_id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        // Confirmar la transacción
+        $conn->commit();
+
+        // Devolver el nuevo número de visitas
+        return $visitas;
+    } catch (Exception $e) {
+        // Revertir la transacción en caso de error
+        $conn->rollBack();
+        echo "Failed: " . $e->getMessage();
+    }
+  $conn = null;
+}
