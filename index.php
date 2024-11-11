1f6e2850d9b32c7322f67687f74199e0229e38ea
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-11-11 06:10:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,7 @@
 </head>
 
 <body>
-  <div id="header">
-    <div id="header_icon"></div>
-    <div id="header_options">
-      <div id="header_options_arboles"></div>
-      <div id="header_options_parques"></div>
-    </div>
-    <div id="header_login"></div>
-  </div>
+  <?= require_once('header.php')?>
   <div id="sidebar">
   </div>
   <div id="index_main">
