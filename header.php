680427e38475f739f6178af730c0fb1a9a23f992
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-11-18 19:05:38 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div id="header">
-    <div id="header_icon"><img id="header_icon_image" src="icono.png"></div>
+    <div id="header_icon"><img id="header_icon_image" src="icono.png" onclick="window.location='arbolesmadrid.es'"></div>
     <div id="header_options">
       <div id="header_options_arboles"><a id="header_options_arboles_link" href="arboles.php">Arboles</a></div>
       <div id="header_options_parques"><a id="header_options_parques_link" href="parques.php">Parques</a></div>
