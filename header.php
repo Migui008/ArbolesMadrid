b8649bde8a31e78bb8558470ce42903bad086c50
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-11-18 18:12:03 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <div id="header">
     <div id="header_icon"><img id="header_icon_image" src="icono.png"></div>
     <div id="header_options">
-      <div id="header_options_arboles">Arboles</div>
-      <div id="header_options_parques">Parques</div>
+      <div id="header_options_arboles"><a id="header_options_arboles_link" href="arboles.php">Arboles</a></div>
+      <div id="header_options_parques"><a id="header_options_parques_link" href="parques.php">Parques</a></div>
     </div>
     <div id="header_login"><a href="login.php"></a></div>
   </div>
