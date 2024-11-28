4e60528845b9bb0883a0e27583476c6f9deaafa2
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-11-29 00:31:20 +0100

    Update editArbol.php

--- editArbol.php
+++ editArbol.php
@@ -83,19 +83,19 @@
   <div id="edit_main">
     <form id="edit_main_form">
       <label class="edit_main_form_label" for="arbolName" name="nombre">Nombre</label>
-      <input class="edit_main_form_input" id="arbolName" type="text" value="<?php $contenidoArbol['nombre'];" ?> >
+      <input class="edit_main_form_input" id="arbolName" type="text" value="<?php echo $contenidoArbol['nombre'];" ?> >
       <label class="edit_main_form_label" for="nombreCientifico" name="nombreCien">Nombre científico</label>
-      <input class="edit_main_form_input" id="nombreCientifico" type="text" value="<?php $contenidoArbol['nombre_cientifico']; ?>" >
+      <input class="edit_main_form_input" id="nombreCientifico" type="text" value="<?php echo $contenidoArbol['nombre_cientifico']; ?>" >
       <label class="edit_main_form_label" for="familia" name="familia">Familia</label>
-      <input class="edit_main_form_input" id="familia" type="text" value="<?php $contenidoArbol['familia']; ?>" >
+      <input class="edit_main_form_input" id="familia" type="text" value="<?php echo $contenidoArbol['familia']; ?>" >
       <label class="edit_main_form_label" for="clase" name="clase">Clase</label>
-      <input class="edit_main_form_input" id="clase" type="text" value="<?php $contenidoArbol['clase']; ?>" >
+      <input class="edit_main_form_input" id="clase" type="text" value="<?php echo $contenidoArbol['clase']; ?>" >
       <?php
         foreach($textoArbol as $textoSeccion){
           echo "<p class='edit_main_form_input_paragraph'>Número</p>";
           echo "<input class='edit_main_form_input_number' type='number' min='1' value='". $textoSeccion['numero'] ."' name='numero".$textoSeccion['numero']."'>";
           echo "<p class='edit_main_form_input_paragraph'>Titulo</p>";
-          echo "<input class='edit_main_form_input' type='text' value='". $textoSeccion['titulo'] ." name='titulo".$textoSeccion['numero']."' >'";
+          echo "<input class='edit_main_form_input' type='text' value='". $textoSeccion['titulo'] ." name='titulo".$textoSeccion['numero']."' >";
           echo "<p class='edit_main_form_input_paragraph'>Titulo inglés</p>";
           echo "<input class='edit_main_form_input' type='text' value='". $textoSeccion['titulo_en'] ."' name='titulo_en".$textoSeccion['numero']."' >";
           echo "<p class='edit_main_form_input_paragraph'>Texto</p>";
