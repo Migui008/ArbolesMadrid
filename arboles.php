8542fa988a11b3779dbfdd0e7e54dd4003a3c6aa
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-12-03 09:50:17 +0100

    Update arboles.php

--- arboles.php
+++ arboles.php
@@ -17,6 +17,7 @@ $arboles_filter['familia']['opciones'] = getOptionsForSelect('familia', 'arboles
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Árboles</title>
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/png" href="image/icono.png">
 </head>
 <body>
     <?php require_once('header.php'); ?>
