080e9fc5b3ecf71311d11ae22a7eafc44d80619c
Migui008 <59126015+Migui008@users.noreply.github.com>
2024-12-03 04:07:03 +0100

    Update common_data.php

--- common_data.php
+++ common_data.php
@@ -1,50 +1,65 @@
 <?php
 $arboles_filter=[
-  "clase" => 
-      ["SELECT DISTINCT a.clase
-      FROM arboles a ORDER BY a.clase;"],
-  "familia" => 
-      ["SELECT DISTINCT a.familia
-      FROM arboles a ORDER BY a.familia;"],
-  "altura" => 
-      ["Menos de 10"=>"SELECT a.nombre, a.id_arbol
-      FROM arboles a WHERE a.altura < 10;",
-      "Entre 10 y 20"=>"SELECT a.nombre, a.id_arbol
-      FROM arboles a WHERE a.altura >= 10 AND a.altura <=20;",
-      "Más de 20"=>"SELECT a.nombre, a.id_arbol
-      FROM arboles a WHERE a.altura > 20;"],
-  "hoja" => 
-      ["SELECT DISTINCT a.hoja
-      FROM arboles a ORDER BY a.hoja;"],
-  "visitas" => 
-      ["Menos de 50"=>"SELECT a.id_arbol, a.nombre
-      FROM arboles a WHERE a.visitas < 50",
-      "Entre 50 y 400"=>"SELECT a.nombre, a.id_arbol
-      FROM arboles a WHERE a.visitas >= 50 AND a.visitas <= 400;",
-      "Más de 400"=>"SELECT a.nombre, a.id_arbol
-      FROM arboles a WHERE a.visitas > 400;"]
+  "clase" =>[
+    "tipo" => "select",
+    "query" => ""
+  ],
+  "familia" =>[
+    "tipo" => "select",
+    "query" => ""
+  ],
+  "visitas" => [
+    "tipo" => "radio",
+    "opciones" => [
+        [
+            "rango" => "Menos de 50",
+            "condicion" => "a.visitas < 50"
+        ],
+        [
+            "rango" => "Entre 50 y 400",
+            "condicion" => "a.visitas >= 50 AND a.visitas <= 400"
+        ],
+        [
+            "rango" => "Más de 400",
+            "condicion" => "a.visitas > 400"
+        ]
+    ]
+  ]
 ];
 
 $parques_filter=[
-  "zona" => 
-      ["SELECT DISTINCT p.zona
-      FROM parques p ORDER BY p.zona;"],
-  "tipo" => 
-      ["SELECT DISTINCT p.tipo
-      FROM parques p ORDER BY p.tipo"], //histórico, urbano, jardín botánico, parque forestal
-  "accesibilidad" => 
-      ["1" => "SELECT p.id_parque, p.nombre
-      FROM parques p WHERE :t1 != null;",
-      "2" => "SELECT p.id_parque, p.nombre
-      FROM parques p WHERE :t1 != null AND :t2 != null;",
-      "3" => "SELECT p.id_parque, p.nombre
-      FROM parques p WHERE :t1 != null AND :t2 != null AND :t3 != null"], //bus, metro, renfe
-  "visitas" =>
-      ["Menos de 50"=>"SELECT p.id_parque, p.nombre
-      FROM parques p WHERE p.visitas < 50",
-      "Entre 50 y 400"=>"SELECT p.nombre, p.id_arbol
-      FROM parques p WHERE p.visitas >= 50 AND p.visitas <= 400;",
-      "Más de 400"=>"SELECT p.nombre, p.id_arbol
-      FROM parques p WHERE p.visitas > 400;"]
-];
+  "accesibilidad" => [
+      "tipo" => "checkbox",
+      "opciones" => [
+        [
+            "transporte" => "Bus",
+            "condicion" => "p.transporte_bus != null"
+        ],
+        [
+            "transporte" => "Metro",
+            "condicion" => "p.transporte_metro != null"
+        ],
+        [
+            "transporte" => "RENFE",
+            "condicion" => "p.transporte_renfe != null"
+        ]
+      ]
+  ]
+  "visitas" => [
+    "tipo" => "radio",
+    "opciones" => [
+        [
+            "rango" => "Menos de 50",
+            "condicion" => "p.visitas < 50"
+        ],
+        [
+            "rango" => "Entre 50 y 400",
+            "condicion" => "p.visitas >= 50 AND p.visitas <= 400"
+        ],
+        [
+            "rango" => "Más de 400",
+            "condicion" => "p.visitas > 400"
+        ]
+    ]
+  ];
 ?>
